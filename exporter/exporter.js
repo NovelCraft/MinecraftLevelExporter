87e0d3a8542481e8430dc2c460b3fc138e45e5d9
Futrime <futrime@outlook.com>
2023-01-28 10:01:35 +0800

    feat(exporter): change to export structure

--- exporter/exporter.js
+++ exporter/exporter.js
@@ -1,6 +1,7 @@
 let cmd = mc.newCommand(
     'export',
-    'Export blocks in a region to a JSON file'
+    'Export blocks in a region to a JSON file',
+    PermType.Console
 )
 
 cmd.mandatory(
@@ -16,78 +17,47 @@ cmd.mandatory(
 cmd.overload(['start', 'end'])
 
 cmd.setCallback((cmd, origin, output, results) => {
-    logger.log('Exporting level data...')
-
-    let startTimestamp = Date.now()
-
     let startPos = results['start']
     let endPos = results['end']
 
     if (startPos == undefined) {
-        return output.error('Start position is null')
+        logger.error('Start position is null')
+        return
     }
 
     if (endPos == undefined) {
-        return output.error('End position is null')
+        logger.error('End position is null')
+        return
     }
 
     if (startPos.dim != endPos.dim) {
-        return output.error('Start and end positions must be in the same dimension')
+        logger.error('Start and end positions must be in the same dimension')
+        return
     }
 
     if (startPos.x > endPos.x || startPos.y > endPos.y || startPos.z > endPos.z) {
-        return output.error('Start position must be less than end position')
+        logger.error('Start position must be less than end position')
+        return
     }
 
-    // Make an empty 3D array
-    let blocks = Array.from(
-        Array(endPos.x - startPos.x),
-        () => Array(endPos.y - startPos.y)
-            .fill().map(
-                () => Array(endPos.z - startPos.z).fill(0)
-            )
-    )
+    logger.log('Exporting level data...')
+    let startTimestamp = Date.now()
 
-    logger.log(`Got ready in ${Date.now() - startTimestamp}ms`)
+    // Get structure
+    let structureNbt = mc.getStructure(startPos, endPos)
 
-    // Fill the array with block IDs
-    let dimid = startPos.dimid
-    let count = 0
-    let lastCount = 0
-    let periodStartTimestamp = Date.now()
-    for (let x = startPos.x; x < endPos.x; x++) {
-        for (let y = startPos.y; y < endPos.y; y++) {
-            for (let z = startPos.z; z < endPos.z; z++) {
-                let blockObj = mc.getBlock(x, y, z, dimid)
-                if (blockObj == null) {
-                    return output.error(`Block at ${x}, ${y}, ${z} is null`)
-                }
-                blocks[x][y][z] = blockObj.id
-                ++count
-                let periodLength = Date.now() - periodStartTimestamp
-                if (periodLength > 1000) {
-                    logger.log(``)
-                    logger.log(`Processed ${count} / ${blocks.length * blocks[0].length * blocks[0][0].length} blocks`)
-                    logger.log(`Current speed: ${(count - lastCount) * 1000 / periodLength} blocks/s`)
-                    logger.log(`Average speed: ${count * 1000 / (Date.now() - startTimestamp)} blocks/s`)
-                    lastCount = count
-                    periodStartTimestamp = Date.now()
-                }
-            }
-        }
-    }
+    // Get JSON string
+    let jsonStr = structureNbt.toString()
 
-    let jsonStr = JSON.stringify(blocks)
+    // Write to file
     File.mkdir('plugins/level_exporter')
-
     if (!File.writeTo('plugins/level_exporter/level_data.json', jsonStr)) {
-        return output.error('Failed to write to plugins/level_exporter/level_data.json')
+        logger.error('Failed to write to plugins/level_exporter/level_data.json')
+        return
     }
 
     logger.log('Successfully exported level data to plugins/level_exporter/level_data.json')
     logger.log(`Took ${Date.now() - startTimestamp}ms`)
-
-    return output.success('Successfully exported level data to plugins/level_exporter/level_data.json')
 })
 
 cmd.setup()
